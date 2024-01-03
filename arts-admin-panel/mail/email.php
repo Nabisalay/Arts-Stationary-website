2d8e3389eb02ffb1e377d2e00d13c4a801bd9e37
Nabisalay <121075018+Nabisalay@users.noreply.github.com>
2024-01-03 05:19:17 +0500

    Update email.php

--- arts-admin-panel/mail/email.php
+++ arts-admin-panel/mail/email.php
@@ -29,7 +29,7 @@ if (isset($_POST['forgetPass'])) {
                 $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
                 $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
                 $mail->Username   = 'salassadiq187@gmail.com';                     //SMTP username
-                $mail->Password   = 'krrp haoo fhom ltee';                               //SMTP password
+                $mail->Password   = 'your smtp password';                               //SMTP password
                 $mail->SMTPSecure = 'TLS';            //Enable implicit TLS encryption
                 $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
